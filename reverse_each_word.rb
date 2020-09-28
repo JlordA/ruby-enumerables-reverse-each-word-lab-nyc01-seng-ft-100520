26ccb0cbc057c242ebbff5be7ed14a9260d9b18b
placid-tier-4550 <james.ardery@gmail.com>
2020-09-28 15:32:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@ require 'pry'
 def reverse_each_word(sentance)
   reverse_sentance = []
   sentance.split(" ")
-binding.pry
-  sentance.each do |word|
+  binding.pry
+  sentance.split.each do |word|
     reverse_sentance << word.reverse
   end
   reverse_sentance.join
